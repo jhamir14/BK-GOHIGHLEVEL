97dc4803cf94abd1214654d53ee4dc126971b20c
camananick-a11y <camananick@gmail.com>
2025-10-02 02:19:05 -0500

    Cambios en urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("create/", views.create_appointment, name="create_appointment"),
     path("list/", views.list_appointments, name="list_appointments"),
-    path("sync/", views.sync_appointments, name="sync_appointments"),
-    path("local/", views.list_local_appointments, name="list_local_appointments"),
+    #path("sync/", views.sync_appointments, name="sync_appointments"),
+    #path("local/", views.list_local_appointments, name="list_local_appointments"),
 ]
