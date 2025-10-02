f4c564810ef7f913e6686840431567d878bfd697
camananick-a11y <camananick@gmail.com>
2025-10-02 02:16:04 -0500

    Agregar admin.py para Appointment con los campos requeridos

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("title", "appointmentId", "calendarId", "contactId", "startTime", "endTime")
+    list_filter = ("calendarId", "locationId", "startTime")
+    search_fields = ("title", "appointmentId", "contactId", "description")
+    ordering = ("-startTime",)
