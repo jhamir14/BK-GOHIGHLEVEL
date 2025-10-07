36c8a4cb5c57fef3d60afe6b8fe2dbbab0902f19
camananick-a11y <camananick@gmail.com>
2025-10-07 02:00:51 -0500

    Modificacion en archivo models.py

--- appointments/models.py
+++ appointments/models.py
@@ -9,6 +9,9 @@ class Appointment(models.Model):
     endTime = models.DateTimeField()
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    
+    # Agregar el campo status
+    status = models.CharField(max_length=50, default="confirmed")  # Este es un campo opcional, con un valor por defecto
 
     def __str__(self):
         return f"{self.title} ({self.startTime} - {self.endTime})"
